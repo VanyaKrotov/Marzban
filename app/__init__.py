abd394750cb65a49dd7766c3530abcf590ac9b42
Ivan Krotov <vanyakrotov3059@gmail.com>
2026-06-19 00:09:05 +0300

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -11,7 +11,7 @@ from fastapi.routing import APIRoute
 
 from config import ALLOWED_ORIGINS, DOCS, XRAY_SUBSCRIPTION_PATH
 
-__version__ = "0.12.3"
+__version__ = "0.12.4"
 
 app = FastAPI(
     title="MarzbanAPI",
